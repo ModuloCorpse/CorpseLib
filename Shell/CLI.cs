86240eec12dac378d03d81df47b35d150c2bfc60
ModuloCorpse <mewen.lereste@wanadoo.fr>
2024-02-17 18:40:14 +0100

    [ADD] Add of shell CLI and abstract commands

---
+++ Shell/CLI.cs
@@ -0,0 +1,22 @@
+﻿namespace CorpseLib.Shell
+{
+    public class CLI
+    {
+        private readonly Dictionary<string, Command> m_Commands = [];
+
+        public OperationResult<string> Execute(string command)
+        {
+            OperationResult<List<string>> splitResult = Helper.SplitCommand(command);
+            if (!splitResult)
+                return new(splitResult.Error, splitResult.Description);
+            string[] args = [..splitResult.Result!];
+            if (args.Length == 0)
+                return new("Command ill-formed", "Command is empty");
+            string commandName = args[0];
+            if (m_Commands.TryGetValue(commandName, out Command? cmd))
+                return cmd.Call(args[1..]);
+            else
+                return new("Unknown command", string.Format("Unknown command {0}", commandName));
+        }
+    }
+}
---
+++ Shell/Command.cs
@@ -0,0 +1,10 @@
+﻿namespace CorpseLib.Shell
+{
+    public abstract class Command(string name)
+    {
+        private readonly string m_Name = name;
+        public string Name => m_Name;
+        internal OperationResult<string> Call(string[] args) => Execute(args);
+        protected abstract OperationResult<string> Execute(string[] args);
+    }
+}

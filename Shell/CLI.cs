b07b191795380c5f3d65384f545c23ecd77a3512
ModuloCorpse <mewen.lereste@wanadoo.fr>
2024-02-18 18:10:06 +0100

    [EDIT] Add of command prefix to CLI

--- Shell/CLI.cs
+++ Shell/CLI.cs
@@ -1,8 +1,19 @@
 ﻿namespace CorpseLib.Shell
 {
-    public class CLI
+    public class CLI(char commandPrefix)
     {
         private readonly Dictionary<string, Command> m_Commands = [];
+        private readonly char m_Prefix = commandPrefix;
+
+        public CLI() : this('\0') { }
+
+        public bool AddCommand(Command command)
+        {
+            if (m_Commands.ContainsKey(command.Name))
+                return false;
+            m_Commands.Add(command.Name, command);
+            return true;
+        }
 
         public OperationResult<string> Execute(string command)
         {
@@ -13,10 +24,16 @@
             if (args.Length == 0)
                 return new("Command ill-formed", "Command is empty");
             string commandName = args[0];
-            if (m_Commands.TryGetValue(commandName, out Command? cmd))
-                return cmd.Call(args[1..]);
+            if (m_Prefix == '\0' || commandName[0] == m_Prefix)
+            {
+                commandName = commandName[1..];
+                if (m_Commands.TryGetValue(commandName, out Command? cmd))
+                    return cmd.Call(args[1..]);
+                else
+                    return new("Unknown command", string.Format("Unknown command {0}", commandName));
+            }
             else
-                return new("Unknown command", string.Format("Unknown command {0}", commandName));
+                return new("Command ill-formed", string.Format("Command '{0}' doesn't start with {1}", commandName, m_Prefix));
         }
     }
 }
